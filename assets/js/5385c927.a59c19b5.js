226dafc11d1fb43ac647c52686b20e1f7a9ab752
Yaroslav <35110747+KurumiFT@users.noreply.github.com>
2023-07-07 00:12:19 +0300

    Built and published by Moonwave

---
+++ assets/js/5385c927.a59c19b5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[8],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/KurumiFT/Devote/blob/master","baseUrl":"/Devote/","classOrder":[],"apiCategories":[]}')}}]);
